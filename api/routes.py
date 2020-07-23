84658e6aaf5e054261c9f4626f0a681aaf78290f
HoanDo <sieunhannhenkutesn@gmail.com>
2020-07-23 19:12:36 +0700

    Update routes.py

--- api/routes.py
+++ api/routes.py
@@ -4,5 +4,5 @@ from controllers.infoController import *
 def initialize_routes(api):
   api.add_resource(UploadImageController,'/api/analysis/image')
   api.add_resource(InformationController,'/api/object/info')
-  api.add_resource(AddInformationController,'/api/object/add')
+  api.add_resource(AddInformationController,'/api/object/action')
 
